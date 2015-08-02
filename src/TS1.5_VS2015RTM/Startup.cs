e0ff03ef5f677882f246a57ebb5a2e55f8db7f5c
mobilemancer <andrkarl@gmail.com>
2015-08-02 11:12:04 +0200

    Added StaticFiles to pipeline

--- src/TS1.5_VS2015RTM/Startup.cs
+++ src/TS1.5_VS2015RTM/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 
@@ -11,12 +7,12 @@ namespace TS1._5_VS2015RTM
     public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        public void ConfigureServices(IServiceCollection services) { }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
